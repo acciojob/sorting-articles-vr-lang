2a95c0b35ebe54133371e6df0ec89891e1ed672b
vr-lang <yuvrajisking66@gmail.com>
2024-11-15 16:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+/your JS code here. If required.
+let list = document.getElementById("band")
+
+const bands = [
+    'Anywhere But Here',
+    'The Bled',
+    'Counterparts',
+    'The Devil Wears Prada',
+    'The Midway State',
+    'Norma Jean',
+    'Oh, Sleeper',
+    'An Old Dog',
+    'Pierce the Veil',
+    'The Plot in You',
+    'Say Anything',
+    'A Skylit Drive',
+    'We Came as Romans'
+  ]
+  
+
+
+  for(let item of bands)
+  {
+    let listItm = document.createElement("li")
+    listItm.innerText = item
+	      list.setAttribute("id", "bands")
+
+    list.appendChild(listItm)
+  }
